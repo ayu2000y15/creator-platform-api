379101089e0b347eac94dc7b3a3d9a7daf488e39
ayu2000y15 <ayu2000y15@nasystem.net>
2025-08-13 14:14:06 +0900

    　　日付：2025/08/12 　変更者：野口 変更内容：デプロイファイル

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
+
+        // CORSミドルウェアが含まれているか確認
+        $middleware->web(append: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
