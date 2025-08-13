c4d75644a92be351da10108ededf84dca197e471
ayu2000y15 <ayu2000y15@nasystem.net>
2025-08-13 13:55:05 +0900

    　　日付：2025/08/12 　変更者：野口 変更内容：デプロイファイル

--- config/cors.php
+++ config/cors.php
@@ -3,8 +3,13 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://main.d2teqaqu3kocnl.amplifyapp.com', 'http://localhost:3000'],
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => [
+        'https://main.d2teqaqu3kocnl.amplifyapp.com',
+        'http://localhost:3000'
+    ],
+    'allowed_origins_patterns' => [
+        '/^https:\/\/.*\.amplifyapp\.com$/', // 全てのAmplifyドメインを許可
+    ],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
